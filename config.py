d5055b46c228f503c38ffb7e00174ba35f97d405
Beulah <beulahagiande@gmail.com>
2024-09-02 17:37:23 +0100

    update

--- config.py
+++ config.py
@@ -4,4 +4,6 @@ class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'supersecretkey')
     MYSQL_HOST = 'localhost'
     MYSQL_USER = 'root'
-    
+    MYSQL_PASSWORD = 'password'
+    MYSQL_DB = 'recs'
+    MYSQL_CURSORCLASS = 'DictCursor'
